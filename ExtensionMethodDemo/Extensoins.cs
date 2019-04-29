dbc87c1f13fb2529a140fdc13bbe37370f5a0d9b
khaled-sameer <khaled.sameer@ysa.gov.ae>
2019-04-29 08:53:16 +0400

    Add project files.

---
+++ ExtensionMethodDemo/Extensoins.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    public static class Extensoins
+    {
+        /**
+         To Turn The Method To an extension Method, simply there are two steps you need to follow:
+            1- first of all make the class which contains the method as a static class.
+            2- already you defined the method as static method but in this step you need to define the class which you need to extend as first parameter,
+               and you should proceed the type name with 'this' KeyWord */
+        public static string ChangeFirstLetter(this string input)
+        {
+            if (input.Length > 0)
+            {
+                char[] charArray = input.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+                return new string(charArray);
+            }
+            return input;
+        }
+    }
+}
---
+++ ExtensionMethodDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Anas";
+            //string result = Extensoins.ChangeFirstLetter(inputString);
+            /**Extension Methods enable you to 'add' methods to existing types without creating a new derived type, recompiling, or otherwise modifying the original type*/
+            /**Extension methods are a special kind of static method, but they are called as if they were instance methods on the extended type.*/
+            string result = inputString.ChangeFirstLetter();
+            Console.WriteLine(result);
+        }
+    }
+}
